d672401559a3da6919edf104e7b95b79fd105d3f
admin <admin@admins-Mac-2.local>
2016-12-21 19:42:45 +0200

    first commit

---
+++ CBHTTPClient/CBHTTPClient.h
@@ -0,0 +1,13 @@
+//
+//  CBHTTPClient.h
+//  CBHTTPClient
+//
+//  Created by admin on 12/21/16.
+//  Copyright © 2016 Vladimir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBHTTPClient : NSObject
+
+@end
---
+++ CBHTTPClient/CBHTTPClient.m
@@ -0,0 +1,13 @@
+//
+//  CBHTTPClient.m
+//  CBHTTPClient
+//
+//  Created by admin on 12/21/16.
+//  Copyright © 2016 Vladimir. All rights reserved.
+//
+
+#import "CBHTTPClient.h"
+
+@implementation CBHTTPClient
+
+@end
